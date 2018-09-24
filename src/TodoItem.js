8a9ec21aad0643ce9de92a573380c8442ff19c3d
alex <chenzeyongjsj@163.com>
2018-09-25 01:00:40 +0800

    PropTypes 数值类型限定

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
 
@@ -8,12 +9,12 @@ class TodoItem extends Component {
     }
 
     render() {
-        const { content } = this.props;
+        const { content, test } = this.props;
         return (
             <Fragment>
                 <div
                     onClick={this.handleClick}>
-                    {content}
+                    {test} - {content}
                 </div>
             </Fragment>
         );
@@ -23,6 +24,19 @@ class TodoItem extends Component {
         const { handleItemDelete, idx } = this.props;
         handleItemDelete(idx)
     }
+}
+
+// 限制传值类型
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired, //isRequired 必须传递
+    content: PropTypes.arrayOf(PropTypes.number, PropTypes.string), // number 或 string
+    handleItemDelete: PropTypes.func,
+    idx: PropTypes.number
+}
+
+// 默认值
+TodoItem.defaultProps = {
+    test: 'hello world'
 }
 
 export default TodoItem;
